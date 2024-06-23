9bf90340f5cdb3777a350fbc4e39a8e8433a9b3d
Katie Ludwig <katierludwig@gmail.com>
2024-06-23 13:42:09 -0600

    yay! completed!

--- api/server.js
+++ api/server.js
@@ -5,6 +5,33 @@ const User = require('./users/model');
 const server = express();
 server.use(express.json());
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id);
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist.'
+        })
+    } else {
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: 'please provide name and bio for the user',
+            })
+        } else {
+            const updatedUser = await User.update(req.params.id, req.body);
+            res.status(200).json(updatedUser)
+        }
+    }
+        } catch(err) {
+            res.status(500).json({
+                message: 'error updating the user',
+                err: err.message,
+                stack: err.stack
+            })
+        }
+    })
+
+
 server.delete('/api/users/:id', async (req, res) => {
     try {
         const possibleUser = await User.findById(req.params.id);
@@ -78,25 +105,6 @@ server.get('/api/users/:id', (req, res) => {
         })
     })
 })
-
- server.put('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
-     .then(user => {
-         if (!user) {
-             res.status(404).json({
-                 message: 'the user with the specified ID does not exist'
-             })
-         }
-         res.json(user);
-     })
-     .catch(err => {
-         res.status(500).json({
-             message: 'error retrieving the user',
-             err: err.message,
-             stack: err.stack
-         })
-     })
- })
 
 server.use('*', (req, res) => {
     res.status(404).json({
