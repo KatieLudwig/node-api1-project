8e2426ddb3a4e85f43d2703e1a5413e420a2f9a9
Katie Ludwig <katierludwig@gmail.com>
2024-06-23 13:35:10 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -6,16 +6,25 @@ const server = express();
 server.use(express.json());
 
 server.delete('/api/users/:id', async (req, res) => {
-    const possibleUser = await User.findById(req.params.id);
-        if (!possibleUser) {
-            res.status(404).json({
-                message: 'The user with the specified ID does not exist.'
+    try {
+        const possibleUser = await User.findById(req.params.id);
+            if (!possibleUser) {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist.'
             })
         } else {
             const deletedUser = await User.remove(possibleUser.id);
             res.status(200).json(deletedUser)
         }
-     })
+    } catch (err) {
+        res.status(500).json({
+            message: 'error deleting the user',
+            err: err.message,
+            stack: err.stack
+        })
+    }
+})
+    
 
 server.post('/api/users', (req, res) => {
     const user = req.body;
